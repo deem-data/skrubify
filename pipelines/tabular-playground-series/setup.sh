da3b9f7305b1d0da49f31b103062109755d337ea
e-strauss <92718421+e-strauss@users.noreply.github.com>
2025-09-16 16:53:23 +0200

    added setup script for dataset + updated pipelines

---
+++ pipelines/tabular-playground-series/setup.sh
@@ -0,0 +1,5 @@
+# Downlaod dataset
+kaggle competitions download -c tabular-playground-series-oct-2022
+
+# Unpack to ./input
+unzip tabular-playground-series-oct-2022.zip -d input/
--- pipelines/tabular-playground-series/tabular-playground-series-oct-2022_SKRUB.py
+++ pipelines/tabular-playground-series/tabular-playground-series-oct-2022_SKRUB.py
@@ -2,7 +2,6 @@ import pandas as pd
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
-from concurrent.futures import ProcessPoolExecutor
 import skrub
 
 from time import time
@@ -19,21 +18,20 @@ t1 = time()
 print("Loaded data schema and starting to read in data in parallel")
 
 # Read and concatenate training data
+file_names = [f"./input/train_{i}.csv" for i in range(10)]
+file_names_skrub = skrub.var("file_names", file_names)
 
-def read_csv_file(i):
-    return pd.read_csv(f"./input/train_{i}.csv", dtype=dtypes)
 
-# parallel read
-with ProcessPoolExecutor() as executor:
-    train_dfs = list(executor.map(read_csv_file, range(10)))
+def read_and_concat_frame(file_names):
+    frames = [pd.read_csv(f_name, dtype=dtypes) for f_name in file_names]
+    single_frame = pd.concat(frames, ignore_index=True)
+    return single_frame
 
-# concatenate
-train_df = pd.concat(train_dfs, ignore_index=True)
+
+train_df = file_names_skrub.skb.apply_func(read_and_concat_frame).skb.subsample(n=100)
+
 t2 = time()
 print("Read and merge train data")
-
-# manually clean up memory
-del train_dfs
 
 # Prepare the data
 X = train_df.drop(
@@ -47,20 +45,28 @@ X = train_df.drop(
         "team_B_scoring_within_10sec",
     ],
     axis=1,
-)
-y = train_df[["team_A_scoring_within_10sec", "team_B_scoring_within_10sec"]]
-
-# Skrub code starts here:
-X_skrub = skrub.X() # init var with empty to avoid preview computation
-y_skrub = skrub.y()
+).skb.mark_as_X()
+y = train_df[["team_A_scoring_within_10sec", "team_B_scoring_within_10sec"]].skb.mark_as_y()
 
 # Train the model for team A
 model_A = lgb.LGBMClassifier(force_col_wise=True)
-preds_A = X_skrub.skb.apply(model_A, y=y_skrub.iloc[:, 0])
+preds_A = X.skb.apply(model_A, y=y.iloc[:, 0])
 # Train the model for team B
 model_B = lgb.LGBMClassifier(force_col_wise=True)
-preds_B = X_skrub.skb.apply(model_B, y=y_skrub.iloc[:, 1])
+preds_B = X.skb.apply(model_B, y=y.iloc[:, 1])
 
+
+def make_result_df(prob_A, prob_B):
+    return pd.DataFrame(
+        {
+            "team_A_scoring_within_10sec": prob_A,
+            "team_B_scoring_within_10sec": prob_B,
+        }
+    )
+
+
+result = preds_A.skb.apply_func(make_result_df, preds_B)
+learner = result.skb.make_learner()
 learner_A = preds_A.skb.make_learner()
 learner_B = preds_B.skb.make_learner()
 t3 = time()
@@ -68,16 +74,18 @@ print("Defined Skrub pipeline")
 
 # Split the data
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
-del X,y
-train_env_dict = {"X" : X_train, "y" : y_train}
-val_env_dict = {"X" : X_val, "y" : y_val}
+del X, y
+train_env_dict = {"X": X_train, "y": y_train}
+val_env_dict = {"X": X_val, "y": y_val}
 
 print("Using Skrub learner for training and prediction")
 
-learner_A.fit(train_env_dict)
-learner_B.fit(train_env_dict)
-prob_A = learner_A.predict_proba(val_env_dict)[:, 1]
-prob_B = learner_B.predict_proba(val_env_dict)[:, 1]
+learner.fit(train_env_dict)
+result_df = learner.predict_proba(val_env_dict)
+# learner_A.fit(train_env_dict)
+# learner_B.fit(train_env_dict)
+# prob_A = learner_A.predict_proba(val_env_dict)[:, 1]
+# prob_B = learner_B.predict_proba(val_env_dict)[:, 1]
 
 # split = preds.skb.train_test_split()
 # learner_A.fit(split["train"])
@@ -94,7 +102,7 @@ val_preds = pd.DataFrame(
 )
 
 # Calculate log loss
-val_log_loss = log_loss(y_val, val_preds) # val_log_loss = log_loss(split["y_test"], val_preds)
+val_log_loss = log_loss(y_val, val_preds)  # val_log_loss = log_loss(split["y_test"], val_preds)
 print(f"Validation Log Loss: {val_log_loss}")
 
 t4 = time()
